e7c341eeb0d153b6dfb6425d5528ce79094f8e49
Diego de Araujo <101844511+Daraujocamara@users.noreply.github.com>
2023-03-18 23:18:21 -0300

    Operadores em java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,73 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);*/
+       /* int numero = 5;
+
+        //numero = numero +1;
+        numero++;
+
+        System.out.println(numero);*/
+
+       /* boolean variavel = true;
+
+        System.out.println(!variavel); */
+
+        /*int a,b;
+
+        a=5;
+        b=6;
+        String resultado ="";
+
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "Falso";*/
+
+           /* int a,b;
+
+            a=6;
+            b=6;
+            String resultado = a==b ? "Verdadeiro" : "Falso";
+
+            System.out.println(resultado); */
+        
+        /*int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("A nossa condição é verdadeira");
+        
+        }
+
+        System.out.println("NumeroUm é igual a NumeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("NumeroUm é diferente de NumeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("NumeroUm é maior que NumeroDois? " + simNao);*/
+
+        
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+            System.out.println("Fim");
+
+
+    }
+    
+}

742a7b958036dc462bec666dd83e16b2e6d0622f
Diego de Araujo <101844511+Daraujocamara@users.noreply.github.com>
2023-03-29 21:40:35 -0300

    calsse-atributo-maetodo

--- Orientacao-Objeto/Carro.java
+++ Orientacao-Objeto/Carro.java
@@ -1,3 +1,52 @@
 public class Carro {
     
+    String cor;
+    String modelo;
+    int capacidadeTanque; 
+
+    //contrutor aqui
+    //sobrecarga de contrutor
+    Carro (){
+
+    }
+
+    Carro (String cor, String modelo, int capacidadeTanque){
+        //colocando valores nos atributos
+        this.cor = cor;// this tem relação com o objeto, diferencia o que pertence ao objeto
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    // get e set tanque
+    void setCapacidadeTanque (int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque(){
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque (double valorCombustivel){
+
+        return  valorCombustivel * capacidadeTanque;
+    }
+     
+    //metodos para encher o tanque
 }
